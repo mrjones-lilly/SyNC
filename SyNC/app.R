0ed30a7f536a5c849b9e02ec282dc5963c88af46
Matthew Jones <jonesmros@gmail.com>
2018-05-02 12:25:44 -0400

    Halfway through UI rendering checkboxes, comments still in

--- SyNC/app.R
+++ SyNC/app.R
@@ -23,6 +23,12 @@ ui <- fluidPage(
 #Please note that the Communication Owner is accountable to Protect Lilly and to share the appropriate level of information with only those who 'need to know'. Refer to Protect Lilly for further guidance on how to classify and manage information."),
       helpText("Welcome to SyNC, a tool to demystify what groups should be informed on topics to which you may be introducing process improvements."),
       
+
+      #Branching requirement 101
+checkboxInput("triage", "View recommended triage forum(s) and/or request to triage a change issue",FALSE),
+checkboxInput("distribution", "View recommended distribution groups and/or send an email communication",FALSE),
+ 
+
       # Input communications owner.  This currently has no functionality, because the from line is spoofable.
       # Replace communications owner with authentication code
       #textInput("text", h3("Communication Owner"), 
@@ -30,16 +36,18 @@ ui <- fluidPage(
       #helpText("Only the person listed as the Communication Owner will be able to send the email. This person is also accountable to the content of the communication as it relates to Protect Lilly."),
       
       # Select topic.  This changes pulls the highly recommended functions list from the back end.
-      selectInput("topic", h3("Communication Topic"),c("Please click 'Update Topic Dropdown'")),
-      helpText("Please select a topic. Selecting a topic will generate a list of highly recommended recipients. If your topic isn't in the list, you may contact synchelp@lilly.com to request that a topic be added.  You may also use any listed topic and manually select your audience from the recommended and non-recommended groups for that topic."),
+      uiOutput("selectTopic"),
+      uiOutput("selectTopicHelp"),
+      #helpText("Please select a topic. Selecting a topic will generate a list of highly recommended recipients. If your topic isn't in the list, you may contact synchelp@lilly.com to request that a topic be added.  You may also use any listed topic and manually select your audience from the recommended and non-recommended groups for that topic."),
       
       # Get current topics
       # Removed end-user ability to update topics for simplicity.  User must start a new session of the app to get any updates.
       #actionButton("refreshTopic", "Update Topic Dropdown"),
       
-      # Recommended groups, dynamically generated in a loop over the lists recommended for the selected topic, each wrapped in their own conditional
+      # Recommended groups, dynamically generated in a loop over the lists recommended for the selected topic, each wrapped in their own conditional ####
       # Always shown.  Default checkbox set to yes (indicating they will be mailed unless the user ops them out)
-      actionButton("viewRec", "Show recommended groups"),
+      uiOutput("viewRecUi"),
+      #actionButton("viewRec", "Show recommended groups"),
       # conditionalPanel(condition = FALSE,groupVector <- c("Rubies")),
       # lapply(1:length(groupVector), function(i) {
       #     checkboxInput(groupVector[i], groupVector[i],TRUE)
@@ -64,7 +72,6 @@ ui <- fluidPage(
       
       uiOutput("recMyNonGroup"),
       
-      
       # Decided to collapse classification for simplicity
       # Show GSS groups
       #actionButton("GSSShow", "Show non-recommended GSS groups"),
@@ -74,7 +81,6 @@ ui <- fluidPage(
       
       # Show Other groups
       #actionButton("otherShow", "Show non-recommended other groups"),
-      
       
       # Mail-based input values
       #textInput("from", "From:", value="jones_matthew_robert@gmail.com"),
@@ -95,7 +101,10 @@ ui <- fluidPage(
 ### Define server logic to send email as specified by the GUI
 server <- function(input, output, session) {
   
-  ### All the non-button code starts here
+  ### All the starter code starts here
+  
+  #Making this part of the fold
+   
   # Pre-populate the topic list. 
   # Removed end-user ability to update topics for simplicity.  User must start a new session of the app to get any topic list updates.
   readIn <- read.table("../SyNClists/topic_list.csv") # Fetching the current topic list
@@ -103,12 +112,57 @@ server <- function(input, output, session) {
   updateSelectInput(session, "topic", label = NULL, choices = choiceVector,  selected = NULL)
   
   # Updating the to: field based on checkbox selection: initial mode.
-  updateTextInput(session, "to", value = paste("I'm an e-mail address"))
+  updateTextInput(session, "to", value = paste("jones_matthew_robert@lilly.com"))
   
   # Somehow get the user's email address when they log in.  Delete the from: field, use this value instead
-
+  # capture user ID as reactive object
+  #user <- reactive({
+  #  if(!is.null(session$user))  {
+  #    my_user <- session$user
+  #  } else {
+      my_user <- Sys.getenv("USER")
+  #  }
+  #  return(my_user)
+  #})
+  usermail <- paste(trimws(my_user),"@lilly.com",sep="")
+       showModal(modalDialog(
+         title = "When using the 'Send' button, email will be sent from:",
+         usermail,
+         easyClose = TRUE,
+         footer = NULL
+      ))  # Debugging
+  
   ### All button code is below here
   
+  #Branching requirement 101
+  #Listen for triage checkbox
+  observe({
+    if(is.null(input$triage) || input$triage==0) return(NULL)
+    #Build the triage section of the form when this checkbox is checked
+    
+      showModal(modalDialog(
+         title = "Do some talking",
+         "Here in the talk section.",
+         easyClose = TRUE,
+         footer = NULL
+      ))  # Talking to the user
+  })
+  
+  #Listen for distribution checkbox
+  observe({
+    if(is.null(input$distribution) || input$distribution==0) return(NULL)
+    #Build the triage section of the form when this checkbox is checked
+    output$selectTopic <- renderUI({selectInput("topic", h3("Communication Topic"),c("Please click 'Update Topic Dropdown'"))})
+    output$selectTopicHelp <- renderUI({helpText("Please select a topic. Selecting a topic will generate a list of highly recommended recipients. If your topic isn't in the list, you may contact synchelp@lilly.com to request that a topic be added.  You may also use any listed topic and manually select your audience from the recommended and non-recommended groups for that topic.")})
+    output$viewRecUi <- renderUI({  actionButton("viewRec", "Show recommended groups")})
+      showModal(modalDialog(
+         title = "Do some talking",
+         "Here in the talk section.",
+         easyClose = TRUE,
+         footer = NULL
+      ))  # Talking to the user
+  })
+       
   # Populate recommended group list
   observe({
     if(is.null(input$viewRec) || input$viewRec==0) return(NULL)
@@ -132,7 +186,13 @@ server <- function(input, output, session) {
     }) # Dynamically generating checkboxes
     
     # Updating the to: field based on checkbox selection: updated recommended topics mode.
-    
+    #updateTextInput(session, "to", value = paste("jones_matthew_robert@lilly.com"))
+    selectedGroup <- "Angie" #isolate(input$group)
+    selectedGroupCSV <- paste(selectedGroup,".csv",sep="")
+    selectedGroupCSVPath <- paste("../SyNClists/",selectedTopicCSV,sep="")
+    readIn <- read.table(selectedGroupCSVPath) # Fetching the current associated list data
+    distVector <- unique(c(t(readIn)))
+    updateTextInput(session, "to", label = NULL, value=distVector[1])
   })
   
   # Updating the to: field based on recommended checkbox selection: checkbox mode.
@@ -163,12 +223,13 @@ server <- function(input, output, session) {
   
   # Updating the to: field based on non-recommended checkbox selection: checkbox mode.
   
-  # Watch the dropdown Communication Topic (topic) to refresh the recommended and non-recommended groups
+  # Watch the dropdown Communication Topic (topic) to refresh the recommended and non-recommended groups ####
     
-  #Send mail button
+  #Send mail button ####
   observe({
     if(is.null(input$send) || input$send==0) return(NULL)
-    from <- "jones_matthew_robert@lilly.com"
+    from <- usermail
+    #from <- "jones_matthew_robert@lilly.com"
     to <- isolate(input$to)
     to2 <- isolate(input$to2)
     subject <- isolate(input$subject)
